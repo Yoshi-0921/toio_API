39d7bd5fc6c2904a36d26be18edad635f57d268f
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2021-04-14 16:35:27 +0900

    property created

---
+++ utils/property.py
@@ -0,0 +1,19 @@
+
+
+class Property:
+    def __init__(self, write, read, notify):
+        self.__write = write
+        self.__read = read
+        self.__notify = notify
+
+    @property
+    def write(self):
+        return self.__write
+
+    @property
+    def read(self):
+        return self.__read
+
+    @property
+    def notify(self):
+        return self.__notify
