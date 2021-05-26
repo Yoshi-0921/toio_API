b5462e256744cfb726d979ee5ecb2a9d6e9bd5a2
Yoshi-0921 <yoshinari.moto@fuji.waseda.jp>
2021-05-26 20:30:44 +0900

    name fixed

--- gui_main.py
+++ gui_main.py
@@ -3,5 +3,5 @@
 from toio_API.utils.tkinter_setup import toioWindow
 
 if __name__ == "__main__":
-    app = toioWindow(toio_names=["Yoshi", "Moto", "Sug1", "Sug2"])
+    app = toioWindow(toio_names=["Yoshi", "Moto"])
     app.mainloop()
